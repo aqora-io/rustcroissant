1a148ea5830a15320b33c621689d3e74a37e5b95
cimandef <angel@pop-os.com>
2026-02-26 23:11:19 +0100

    fix: skip serializing agent attribute if None

--- src/specs/agent.rs
+++ src/specs/agent.rs
@@ -4,14 +4,16 @@ use schematic::Config;
 crate::config_struct!(
     #[derive(Config)]
     pub struct Agent {
-        #[serde(rename = "@type")]
+        #[serde(rename = "@type", skip_serializing_if = "Option::is_none")]
         pub r#type: Option<NonEmptyString>,
 
-        #[serde(rename = "@id")]
+        #[serde(rename = "@id", skip_serializing_if = "Option::is_none")]
         pub id: Option<Id>,
 
+        #[serde(skip_serializing_if = "Option::is_none")]
         pub name: Option<NonEmptyString>,
 
+        #[serde(skip_serializing_if = "Option::is_none")]
         pub url: Option<url::Url>,
     }
 );
